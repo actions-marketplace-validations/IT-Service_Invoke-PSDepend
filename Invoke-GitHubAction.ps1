f8dde59099e28851c0b92f57ed13db07f639d07e
Sergei S Betke <sergey.s.betke@yandex.ru>
2022-03-09 00:51:10 +0300

    update(action): Update template for PowerShell actions

--- Invoke-GitHubAction.ps1
+++ Invoke-GitHubAction.ps1
@@ -1,42 +1,82 @@
 #!/usr/bin/env pwsh
+Import-Module $PSScriptRoot/lib/GitHubActionsCore;
 
-$Param = Get-ActionInput 'param-id';
-
-$installModParams = @{ Name = 'Pester'; Force = $true };
-
-if ($Version)
+try
 {
-	$installModParams.Add('RequiredVersion', $Version)
-};
+	Enter-ActionOutputGroup -Name 'Prepare repositories';
+	try
+	{
+		Set-PSRepository -Name PSGallery -InstallationPolicy Trusted;
+		Register-PackageSource -Name 'nuget.org' -Location 'https://api.nuget.org/v3/index.json' -ProviderName NuGet -Trusted -Force | Out-Null;
+	}
+	finally
+	{
+		Exit-ActionOutputGroup;
+	};
 
-Write-ActionInfo 'checking for Pester module...'
+	Enter-ActionOutputGroup -Name 'Install PSDepend';
+	try
+	{
+		$ModuleName = 'PSDepend';
 
-$modules = Get-Module -ListAvailable Pester;
-$requiredPester = $null
-if ( $Version )
-{
-	$requiredPester = $modules | Where-Object { $_.Version -eq $Version };
+		$installModuleParams = @{ Name = $ModuleName; Force = $true };
+		$VersionParam = ( Get-ActionInput 'version' );
+		if ( $VersionParam -and ( $VersionParam -ne 'latest' ) )
+		{
+			$Version = $VersionParam;
+		};
+
+		if ( $Version )
+		{
+			$installModParams.Add( 'RequiredVersion', $Version );
+		};
+
+		Write-ActionInfo ( 'checking for {0} module...' -f $ModuleName );
+
+		$modules = Get-Module -ListAvailable -Name $ModuleName;
+		$requiredModule = $null;
+		if ( $Version )
+		{
+			$requiredModule = $modules | Where-Object { $_.Version -eq $Version };
+		}
+		else
+		{
+			$requiredModule = $modules | Sort-Object Version | Select-Object -Last 1;
+		};
+
+		if ( $requiredModule )
+		{
+			Write-ActionInfo ('{0} module version {1} already installed.' -f $requiredModule.Name, $requiredModule.Version);
+		}
+		else
+		{
+			Write-ActionInfo ( 'installing {0} module...' -f $ModuleName );
+			$ProgressPreference = 'SilentlyContinue';
+			$requiredModule = Install-Module @installModuleParams -PassThru;
+			Write-ActionInfo ('{0} module version {1} installed.' -f $requiredModule.Name, $requiredModule.Version);
+		};
+
+		Import-Module @installModuleParams;
+	}
+	finally
+	{
+		Exit-ActionOutputGroup;
+	};
+
+	$recursiveParam = ( Get-ActionInput -Name 'recurse' );
+	$recursive = -not ( $recursiveParam -and ( $recursiveParam -ne 'true' ) );
+	$verboseParam = ( Get-ActionInput -Name 'verbose' );
+	$verbose = -not ( $verboseParam -and ( $verboseParam -ne 'true' ) );
+
+	Invoke-PSDepend `
+		-Recurse:$recursive `
+		-Confirm:$false `
+		-Verbose:$verbose;
 }
-else
+catch
 {
-	$requiredPester = $modules | Sort-Object Version | Select-Object -Last 1;
-};
-
-if ( $requiredPester )
-{
-	Write-ActionInfo ('Pester module version {0} already installed.' -f $requiredPester.Version);
+	Set-ActionOutput 'error' $_.ToString();
+	$ErrorView = 'NormalView';
+	Set-ActionFailed ($_ | Out-String);
 }
-else
-{
-	Write-ActionInfo 'installing Pester module {0}...' -f "version: ${version}";
-	$ProgressPreference = 'SilentlyContinue';
-	Install-Module @installModParams;
-}
-
-$importedModule = Import-Module @installModParams -PassThru;
-
-Write-ActionInfo ("running Pester version {0} on '$script'" -f $importedModule.Version);
-
-Set-ActionOutput -Name 'random-number' -Value 'output-value';
-
-Write-ActionInfo ("Output info");
+exit [System.Environment]::ExitCode;
